a0fe7ff6e68db15678ee075952a885ff47956ef2
Fernando González <rmfernando@outlook.com>
2019-10-12 19:58:19 -0600

    CLI created.

---
+++ cli.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.lang.reflect.*;
+
+public class cli{
+
+    public static void main(String[] args ){
+
+        for(int i = 0; i<args.length; i++ ){
+
+            if(args[i].equals("-target")){
+                String stage = args[i+1];
+                //System.out.println(stage);
+
+                if(stage.equals("scan")){
+                    System.out.println("This should run and compile Scanner...");
+                }else if(stage.equals("parser")){
+                    System.out.println("This should run and compile parser...");
+
+                }else if(stage.equals("-help") || stage.equals("-h")){
+                    System.out.println("This should open the help cli...");
+                }else{
+                    System.out.println("Argument not valid.");
+                }
+
+            
+            }else if(args[i].equals(null)){
+                // Se instancia el scanner, por ser el default
+                //Scannerc lexer = new Scannerc();
+                //lexer.main(new String[0]);
+                //System.out.println(lexer.token_stream);
+                System.out.println("test");
+            }
+
+        }
+
+    }
+
+
+
+}
+
+/*
+ * 
+ * String one = args[0]; // =="one" //String two = args[1]; // =="two"
+ * 
+ * if(one.equals("-target")){ System.out.println("Succes!"); }
+ */
