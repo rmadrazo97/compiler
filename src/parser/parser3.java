8e529ace460cb3e02fea30908f4354f545d1d8e6
José Alejandro Guzmán Zamora <35410063+JoseGuzmanZamora@users.noreply.github.com>
2019-10-13 00:45:04 -0600

    nuevo parser

--- src/parser/parser3.java
+++ src/parser/parser3.java
@@ -134,7 +134,10 @@ public class parser3 {
     public static boolean method_decl(ArrayList<token> tokens){
         int inicio = contador; 
         
-        if(type(tokens)){
+        if(type(tokens) || tokens.get(contador).value.equals("void")){
+            if(tokens.get(contador).value.equals("void")){
+                contador += 1;
+            }
             if(id(tokens)){
                 if(tokens.get(contador).value.equals("(")){
                     System.out.println("Parseado: (");
@@ -290,7 +293,7 @@ public class parser3 {
         
         String value = tokens.get(contador).value;
         
-        if(value.equals("int") || value.equals("boolean") || value.equals("void")){
+        if(value.equals("int") || value.equals("boolean")){
             System.out.println("Parseado: " + value);
             contador += 1;
             return true;
@@ -487,6 +490,425 @@ public class parser3 {
             return true;
         }else{
             return false;
+        }
+    }
+
+    public static boolean method_call(ArrayList<token> tokens){
+        int inicio = contador; 
+        
+        if(tokens.get(contador).value.equals("callout")){
+            System.out.println("Parseado: callout");
+            contador += 1;
+            
+            if(tokens.get(contador).value.equals("(")){
+                System.out.println("Parseado: (");
+                contador += 1; 
+                
+                if(string_literal(tokens)){
+                    if(tokens.get(contador).value.equals(")")){
+                        System.out.println("Parseado: )");
+                        contador += 1; 
+                        
+                        return true;
+                    }else{
+                        if(tokens.get(contador).value.equals(",")){
+                            if(callout_arg(tokens)){
+                                boolean continuar = true;
+                                while(continuar){
+                                    if(tokens.get(contador).value.equals(",")){
+                                        System.out.println("Parseado: ,");
+                                        contador += 1;
+                                        continuar = callout_arg(tokens);
+                                    }else{
+                                        break;
+                                    }
+                                }
+                                
+                                if(tokens.get(contador).value.equals(")")){
+                                    System.out.println("Parseado: )");
+                                    contador += 1; 
+                                    return true;
+                                }else {
+                                    contador = inicio; 
+                                    return false;
+                                }
+                            }else{
+                                contador = inicio; 
+                                return false; 
+                            }
+                        }else{
+                            contador = inicio; 
+                            return false; 
+                        }
+                    }
+                }else {
+                    contador = inicio; 
+                    return false;
+                }
+            }else {
+                contador = inicio; 
+                return false;
+            }
+        }else{
+            if(method_name(tokens)){
+                if(tokens.get(contador).value.equals("(")){
+                    System.out.println("Parseado: (");
+                    contador += 1;
+                
+                    if(tokens.get(contador).value.equals(")")){
+                        System.out.println("Parseado: )");
+                        contador += 1;
+                        return true;
+                    }else{
+                        if(expr(tokens)){
+                            boolean continuar = true;
+                            while(continuar){
+                                if(tokens.get(contador).value.equals(",")){
+                                    System.out.println("Parseado: ,");
+                                    contador += 1;
+                                    continuar = expr(tokens);
+                                }else{
+                                    break;
+                                }
+                            }
+                            
+                            if(tokens.get(contador).value.equals(")")){
+                                System.out.println("Parseado: )");
+                                contador += 1; 
+                                return true;
+                            }else {
+                                contador = inicio; 
+                                return false;
+                            }
+                        }else{
+                            contador = inicio; 
+                            return false;
+                        }
+                    }
+                }else {
+                    contador = inicio; 
+                    return false;
+                }
+            }else {
+                contador = inicio; 
+                return false;
+            }
+        }
+    }
+
+    public static boolean method_name(ArrayList<token> tokens){
+        return id(tokens);
+    }
+    
+    public static boolean location(ArrayList<token> tokens){
+        int inicio = contador; 
+        if(id(tokens)){
+            if(tokens.get(contador).value.equals("[")){
+                System.out.println("Parseado [");
+                contador += 1; 
+                
+                if(expr(tokens)){
+                    if(tokens.get(contador).value.equals("]")){
+                        System.out.println("Parseado ]");
+                        contador += 1;
+                        
+                        return true;
+                    }else{
+                        contador = inicio; 
+                        return false; 
+                    }
+                }else{
+                    contador = inicio; 
+                    return false;
+                }
+            }else{
+                return true; 
+            }
+        }else{
+            contador = inicio; 
+            return false;
+        }
+    }
+
+    public static boolean expr(ArrayList<token> tokens){
+        int inicio = contador; 
+        if(tokens.get(contador).value.equals("-")){
+            System.out.println("Parseado -");
+            contador += 1; 
+            
+            if(expr(tokens)){
+                if(expr_alt(tokens)){
+                    return true;
+                }else{
+                    contador = inicio; 
+                    return false;
+                }
+            }else{
+                contador = inicio; 
+                return false;
+            }
+        }else if(tokens.get(contador).value.equals("!")){
+            System.out.println("Parseado !");
+            contador += 1;
+            
+            if(expr(tokens)){
+                if(expr_alt(tokens)){
+                    return true;
+                }else{
+                    contador = inicio; 
+                    return false;
+                }
+            }else{
+                contador = inicio; 
+                return false; 
+            }
+        }else if(tokens.get(contador).value.equals("(")){
+            if(expr(tokens)){
+                if(tokens.get(contador).value.equals(")")){
+                    System.out.println("Parseado )");
+                    contador += 1; 
+                    
+                    if(expr_alt(tokens)){
+                        return true; 
+                    }else{
+                        contador = inicio; 
+                        return false; 
+                    }
+                }else{
+                    contador = inicio; 
+                    return false; 
+                }
+            }else{
+                contador = inicio; 
+                return false; 
+            }
+        }else if(location(tokens)){
+            if(expr_alt(tokens)){
+                return true; 
+            }else{
+                contador = inicio;
+                return false;
+            }
+        }else if(method_call(tokens)){
+            if(expr_alt(tokens)){
+                return true; 
+            }else{
+                contador = inicio;
+                return false;
+            }
+        }else if(literal(tokens)){
+            if(expr_alt(tokens)){
+                return true; 
+            }else{
+                contador = inicio;
+                return false;
+            }
+        }else{
+            contador = inicio; 
+            return false;
+        }
+    }  
+
+    public static boolean expr_alt(ArrayList<token> tokens){
+        int inicio = contador;
+        if(bin_op(tokens)){
+            if(expr(tokens)){
+                if(expr_alt(tokens)){
+                    return true; 
+                }else{
+                    contador = inicio; 
+                    return false;
+                }
+            }else{
+                contador = inicio; 
+                return false;
+            }
+        }else{
+            contador = inicio; 
+            return true;
+        }
+    }
+
+    public static boolean callout_arg(ArrayList<token> tokens){
+        if(expr(tokens)){
+            return true;
+        }else  if(string_literal(tokens)){
+            return true;
+        }else{
+            System.out.println("Error");        
+            return false;
+        }
+    }
+
+    public static boolean bin_op(ArrayList<token> tokens){
+        if (arith_op(tokens)){
+            return true;
+        } else  if (rel_op(tokens)){
+            return true;
+        } else if (eq_op(tokens)){
+            return true;
+        } else if (cond_op(tokens)){
+            return true;
+        } else {
+            System.out.println("Error");        
+            return false;
+        }
+    }
+
+    public static boolean arith_op(ArrayList<token> tokens){
+        if(tokens.get(contador).value.equals("+")){
+            contador +=1;
+            System.out.println("Parseado: +");
+            return true;
+        }else if (tokens.get(contador).value.equals("-")){
+            contador +=1;
+            System.out.println("Parseado: -");
+            return true; 
+        }else if (tokens.get(contador).value.equals("*")){
+            contador +=1;
+            System.out.println("Parseado: *");
+            return true;
+        }else if (tokens.get(contador).value.equals("/")){
+            contador +=1;
+            System.out.println("Parseado: /");
+            return true;
+        }else if (tokens.get(contador).value.equals("%")){
+            contador +=1;
+            System.out.println("Parseado: %");
+            return true;
+        }else {
+            System.out.println("Error");
+            return false;
+        }
+    }
+
+    public static boolean rel_op(ArrayList<token> tokens){
+
+        if(tokens.get(contador).value.equals("<")){
+            contador +=1;
+            System.out.println("Parseado: <");
+            return true;
+        } else if (tokens.get(contador).value.equals(">")){
+            contador +=1;
+            System.out.println("Parseado: >");
+            return true; 
+        }else if (tokens.get(contador).value.equals("<=")){
+            contador +=1;
+            System.out.println("Parseado: <=");
+            return true; 
+        } else if (tokens.get(contador).value.equals(">=")){
+            contador +=1;
+            System.out.println("Parseado: >=");
+            return true; 
+        } else {
+            System.out.println("Error");
+            return false;
+        }      
+    }
+
+    public static boolean eq_op(ArrayList<token> tokens){
+        if(tokens.get(contador).value.equals("==")){
+            contador +=1;
+            System.out.println("Parseado: ==");
+            return true;
+        } else if (tokens.get(contador).value.equals("!=")){
+            contador +=1;
+            System.out.println("Parseado: !=");
+            return true;
+        } else {
+            System.out.println("Error");
+            return false;
+        }
+    }
+
+    public static boolean cond_op(ArrayList<token> tokens){
+        if(tokens.get(contador).value.equals("&&")){
+            contador +=1;
+            System.out.println("Parseado: &&");
+            return true;
+        } else if (tokens.get(contador).value.equals("||")){
+            contador +=1;
+            System.out.println("Parseado: ||");
+            return true;
+        } else {
+            System.out.println("Error");
+            return false;
+        }
+    }
+
+    public static boolean literal(ArrayList<token> tokens){
+        if (int_literal(tokens)){      
+            return true;
+        } else  if (char_literal(tokens)){
+            return true;
+        } else  if (bool_literal(tokens)){
+            return true;
+        } else {
+            System.out.println("Error");        
+            return false;
+        }
+    }
+
+    public static boolean id(ArrayList<token> tokens){
+        if (tokens.get(contador).type.equals("Identifier")){
+            System.out.println("Parseado type: <Identifier>");  
+            contador += 1;      
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean char_literal(ArrayList<token> tokens){
+        if(tokens.get(contador).type.equals("Char_Literal")){
+            System.out.println("Parseado " + tokens.get(contador).value);
+            contador += 1;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean int_literal(ArrayList<token> tokens){
+        if (tokens.get(contador).type.equals("DECIMAL")){
+            System.out.println("Parseado type: <int>");  
+            contador += 1;      
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hex_literal(ArrayList<token> tokens){
+        if (tokens.get(contador).type.equals("HEXADECIMAL")){
+            System.out.println("Parseado type: <hex>");  
+            contador += 1;      
+            return true;
+        } else {
+            return false;
+        }
+
+}
+
+    public static boolean string_literal(ArrayList<token> tokens){
+        if (tokens.get(contador).type.equals("String_Literal")){
+            System.out.println("Parseado type: <String_Literal>");  
+            contador += 1;      
+            return true;
+        } else {
+            return false;
+        }
+}
+
+    public static boolean bool_literal(ArrayList<token> tokens){
+        if (tokens.get(contador).value.equals("true") || tokens.get(contador).value.equals("false") ){
+            System.out.println("Parseado type: <bool>");  
+            contador += 1;      
+            return true;
+        } else {
+   
+            return false;
+   
         }
     }
 }
