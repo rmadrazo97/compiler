442f8e111ca3d9d74f7dce3a3665aa22d4044f16
jmadrazo7 <jmadrazo7@penguin>
2019-10-14 20:11:58 -0600

    funciones para tabla

--- src/parser/semantic.java
+++ src/parser/semantic.java
@@ -13,36 +13,80 @@ public class semantic {
     }
     
     public static void recorrer(parse_pointer padre){       
-        if(padre.children.size() > 0){
-            declaraciones(padre);
+        
+        scope(padre);
+        
+        if(padre.children.size() > 0){            
             for(int i = 0; i < padre.children.size(); i++){
                 if(padre.children.get(i).children.size() > 0){
+                    
                     recorrer(padre.children.get(i));
+                    
+                    if (padre.children.get(i).value.equals("type") && padre.children.get(i+1).value.equals("id") ){
+                        
+                        if (padre.value.equals("var_decl")){
+                            // insert to table. 
+                            // insertar todos los id's separados por comas hasta ;
+                            insertId_var_decl(padre, scope);
+
+                        } else {
+                            // insertar el siguiente type y el siguiente id a la tabla; 
+                            insertId(padre, scope);
+                        }
+                    }
                 }
+
             }
         }
     }
     
     public static void declaraciones(parse_pointer padre){
-        if (padre.puntero.value.equals("type")){
-            for(int i = 0; i < padre.children.size(); i++){
-                if(padre.children.get(i).puntero.value.equals("id")){
+        
+        for(int i = 0; i < padre.children.size(); i++){
+            if(padre.children.get(i).puntero.value.equals("id")){
 
-                    // push to table.]
-                    int newRow = symbolTbl.length;
-                    symbolTbl[][].add() 
+                // push to table.
+               
 
-                } else if (padre.children.get(i).puntero.value.equals(",")){
+            } else if (padre.children.get(i).puntero.value.equals(",")){
 
-                    // ignore
+                // ignore
+                continue;
 
-                } else if (padre.children.get(i).puntero.value.equals(";")){
-                    // break
+            } else if (padre.children.get(i).puntero.value.equals(";")){
+                
+                // break
+                break;
+            }
+        } 
 
-                }
-            } 
+    }
+
+    public static void insertId_var_decl(parse_pointer padre, scope){
+        for(int i = 1; i < padre.children.size(); i++){
+            
+            if(!padre.children.get(i).value.equals(";") && !padre.children.get(i).value.equals(",") ){
+                
+                // insert to table
+                // type => padre.children.get(0);
+                // value => padre.children.get(i);
+                // table -> array_list
+                
+
+            } else {
+                continue;
+            }
         }
-  
+    }
+
+    public static void InsertId (parse_pointer padre, scope){
+
+    }
+
+    public static void scope (parse_pointer padre){
+        if (padre.value.equals("block")){
+            scope +=1;
+        }
     }
     
 }
