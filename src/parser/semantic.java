fd4b19f3f952b262d9a7c295e5b1927e306629fc
José Alejandro Guzmán Zamora <35410063+JoseGuzmanZamora@users.noreply.github.com>
2019-10-15 08:19:17 -0600

    Continuacion semantic

--- src/parser/semantic.java
+++ src/parser/semantic.java
@@ -1,89 +1,60 @@
 package parser;
 
-import sun.text.normalizer.SymbolTable;
+import java.util.UUID;
 
 /**
  *
  * @author joseg
  */
-public class semantic {
+public class Semantic {
+    public static int scope = 0;
+    
     
     public static void main(String[] args){
         recorrer(parser3.father);
     }
     
     public static void recorrer(parse_pointer padre){       
-        
-        scope(padre);
+        scopef(padre);
         
         if(padre.children.size() > 0){            
             for(int i = 0; i < padre.children.size(); i++){
-                if(padre.children.get(i).children.size() > 0){
-                    
-                    recorrer(padre.children.get(i));
-                    
-                    if (padre.children.get(i).value.equals("type") && padre.children.get(i+1).value.equals("id") ){
-                        
-                        if (padre.value.equals("var_decl")){
-                            // insert to table. 
-                            // insertar todos los id's separados por comas hasta ;
-                            insertId_var_decl(padre, scope);
+                if (padre.children.get(i).value.equals("type") && padre.children.get(i+1).value.equals("id") ){
+                    if (padre.value.equals("var_decl")){
+                        // insert to table. 
+                        insertId_var_decl(padre);
 
-                        } else {
-                            // insertar el siguiente type y el siguiente id a la tabla; 
-                            insertId(padre, scope);
-                        }
+                    }else {
+                        // insertar el siguiente type y el siguiente id a la tabla; 
+                        insertId(padre, i);
                     }
                 }
-
+                recorrer(padre.children.get(i));
             }
         }
     }
     
-    public static void declaraciones(parse_pointer padre){
-        
-        for(int i = 0; i < padre.children.size(); i++){
-            if(padre.children.get(i).puntero.value.equals("id")){
-
-                // push to table.
-               
-
-            } else if (padre.children.get(i).puntero.value.equals(",")){
-
-                // ignore
-                continue;
-
-            } else if (padre.children.get(i).puntero.value.equals(";")){
-                
-                // break
-                break;
-            }
-        } 
-
-    }
-
-    public static void insertId_var_decl(parse_pointer padre, scope){
+    public static void insertId_var_decl(parse_pointer padre){
         for(int i = 1; i < padre.children.size(); i++){
-            
             if(!padre.children.get(i).value.equals(";") && !padre.children.get(i).value.equals(",") ){
-                
                 // insert to table
-                // type => padre.children.get(0);
-                // value => padre.children.get(i);
-                // table -> array_list
-                
-
-            } else {
-                continue;
+                String iden = UUID.randomUUID().toString();
+                System.out.printf("%30s%7s%7s%15s\n", iden ,padre.children.get(0).children.get(0).value,padre.children.get(i).children.get(0).value,scope);
             }
         }
     }
 
-    public static void InsertId (parse_pointer padre, scope){
+    public static void insertId(parse_pointer padre, int contador){
+        String iden = UUID.randomUUID().toString();
+        if(padre.value.equals("method_decl_alt")){
+            System.out.printf("%30s,%7s%7s%15s\n", iden,padre.children.get(contador).children.get(0).value,padre.children.get(contador+1).children.get(0).value,scope + 1);
+        }else{
+            System.out.printf("%30s%7s%7s%15s\n", iden,padre.children.get(contador).children.get(0).value,padre.children.get(contador+1).children.get(0).value,scope);
+        }
 
     }
-
-    public static void scope (parse_pointer padre){
+    
+    public static void scopef (parse_pointer padre){
         if (padre.value.equals("block")){
             scope +=1;
         }
